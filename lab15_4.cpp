d30097a4db4e418dc483a9451789e88fccf4a801
Nemo <nemonapatsadon@gmail.com>
2026-02-03 02:28:41 +0700

    lab15-4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,21 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a, int &b, int &c, int &d) {
+    int temp[] = {a, b, c, d};
+    int n = 4; 
+
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        
+        int hold = temp[i];
+        temp[i] = temp[j];
+        temp[j] = hold;
+    }
+
+    a = temp[0];
+    b = temp[1];
+    c = temp[2];
+    d = temp[3];
+}
